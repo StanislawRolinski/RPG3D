13dde4ec8bf3c961ccedfdd58402ec7956bcd2b1
StanislawRolinski <stasrolinskyi@gmail.com>
2019-07-10 23:36:15 +0200

    Saving system implemintation prototype added

---
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace RPG.Saving
+{
+    public class SavingSystem : MonoBehaviour
+    {
+        public void Save(string saveFile)
+        {
+            print("Saving to " + saveFile);
+        }
+
+        public void Load(string saveFile)
+        {
+            print("Loading from " + saveFile);
+        }
+
+        private string GetPathFromSaveFile(string saveFile)
+        {
+            return Path.Combine(Application.persistentDataPath, saveFile + ".sav");
+        }
+    }
+}
+
